63e06364a3953b0d34103f781ab39587ae387cbd
JubayerAhmedSamrat <jasamrat@gmail.com>
2024-09-25 14:23:17 +0600

    4th

--- withBreak/3.js
+++ withBreak/3.js
@@ -2,7 +2,7 @@
 
 // with for loop---------------------------------------------------------------
 // for(let num = 1; num <=100; num++){
-//     if(Math.sqrt(num) % 1 === 0 && num !== 1){
+//     if(Math.sqrt(num) % 1 === 0){
         
 //         console.log(num);
 //         break;
@@ -12,7 +12,7 @@
 // with while loop------------------------------------------------------------
 let num = 1;
 while(num <= 100){
-    if(Math.sqrt(num) % 1 === 0 && num !== 1){
+    if(Math.sqrt(num) % 1 === 0){
         console.log(num);
         break;
     }
